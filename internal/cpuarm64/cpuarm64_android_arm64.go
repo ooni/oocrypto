58b6ccb6a0a8ced62113a497e4aa93657467b544
Simone Basso <bassosimone@gmail.com>
2021-11-10 11:09:48 +0100

    feat(internal/cpuarm64): better HWCAP accessor for android/arm64
    This diff pulls code from https://go-review.googlesource.com/c/sys/+/197540/
    to add better support for detecting arm64 CPU features.
    
    The idea here is to use this better proxy functionality for arm64 to feed
    crypto libraries with better understanding of cpu capabilities.
    
    The original issue describing this problem in its full extent was
    https://github.com/ooni/probe/issues/1444.
    
    The https://github.com/ooni/go/commit/78076671c721d679ea8cbee990ee4d98b9da6181
    diff originally solved this problem for the github.com/ooni/go fork
    and the branch tracking go1.17.x development.
    
    AFAICT, as of go1.17, the src/runtime/os_linux.go still falls back to
    reduced functionality in case /proc/self/auxv isn't readable:
    
    https://github.com/golang/go/blob/go1.17.3/src/runtime/os_linux.go#L216
    
    The current issue describing this problem as of go1.17 is
    https://github.com/ooni/probe/issues/1863.

---
+++ internal/cpuarm64/cpuarm64_android_arm64.go
@@ -0,0 +1,79 @@
+//go:build android && arm64
+// +build android,arm64
+
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// This file is based on the diff available at
+// https://go-review.googlesource.com/c/sys/+/197540/
+
+package cpuarm64
+
+/*
+#include <sys/auxv.h>
+
+// getauxval is not available on Android until API level 20. Link it as a weak
+// symbol and check whether it is not NULL before using it.
+unsigned long getauxval(unsigned long type) __attribute__((weak));
+*/
+import "C"
+
+import "sync"
+
+// These constants are Linux specific.
+const (
+	_AT_HWCAP = 16 // hardware capability bit vector
+)
+
+// get returns the value of the getauxval auxiliary vector, or
+// zero where the functionality is unavailable.
+func get(t uint) uint {
+	if C.getauxval == C.NULL {
+		return 0
+	}
+	return uint(C.getauxval(C.ulong(t)))
+}
+
+// dogethwcap returns the value of _AT_HWCAP by calling
+// the getauxval(3) function in libc (if available).
+func dogethwcap() uint {
+	return get(_AT_HWCAP)
+}
+
+// These variables allow to cache getauxval(3) results.
+var (
+	once  sync.Once
+	hwcap uint
+)
+
+// gethwcap is like dogethwcap except that this function
+// ensures we call getauxval(3) just once. After the first
+// invocation we memoize the result.
+func gethwcap() uint {
+	once.Do(func() {
+		hwcap = dogethwcap()
+	})
+	return hwcap
+}
+
+// HWCAP bits. These are exposed by Linux.
+const (
+	hwcap_AES     = 1 << 3
+	hwcap_PMULL   = 1 << 4
+	hwcap_SHA1    = 1 << 5
+	hwcap_SHA2    = 1 << 6
+	hwcap_CRC32   = 1 << 7
+	hwcap_ATOMICS = 1 << 8
+	hwcap_CPUID   = 1 << 11
+)
+
+// HasAES returns whether the CPU supports AES.
+func HasAES() bool {
+	return (gethwcap() & hwcap_AES) != 0
+}
+
+// HasPMULL returns whether the CPU supports PMULL.
+func HasPMULL() bool {
+	return (gethwcap() & hwcap_PMULL) != 0
+}
---
+++ internal/cpuarm64/cpuarm64_otherwise.go
@@ -0,0 +1,16 @@
+//go:build !android || !arm64
+// +build !android !arm64
+
+package cpuarm64
+
+import "golang.org/x/sys/cpu"
+
+// HasAES returns whether the CPU supports AES.
+func HasAES() bool {
+	return cpu.ARM64.HasAES
+}
+
+// HasPMULL returns whether the CPU supports PMULL.
+func HasPMULL() bool {
+	return cpu.ARM64.HasPMULL
+}

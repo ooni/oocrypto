b2bbb689a7ee46e868f6143f4c3f7b6a2ed3bff3
Simone Basso <bassosimone@gmail.com>
2023-05-31 10:10:17 +0200

    fix(merge.bash): delete the correct branch

--- tools/merge.bash
+++ tools/merge.bash
@@ -2,7 +2,7 @@
 set -euxo pipefail
 git checkout stable
 git remote add golang git@github.com:golang/go.git || git fetch golang
-git branch -D golang-upstream golang-http-upstream merged-stable || true
+git branch -D golang-upstream golang-crypto-upstream merged-stable || true
 git fetch golang
 git checkout -b golang-upstream $(cat UPSTREAM)
 git subtree split -P src/crypto/ -b golang-crypto-upstream
